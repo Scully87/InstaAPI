490fc9c45490d6b98c27d892b3a5722b17a80cdb
Scully87 <scully87.ts@googlemail.com>
2015-01-15 12:30:23 +0000

    redirecting to authorize screen successful

--- app/server.rb
+++ app/server.rb
@@ -9,7 +9,7 @@ set :public_folder, Proc.new { File.join(root, '..', 'public') }
 
 enable :sessions
 
-CALLBACK_URL = "http://localhost:9292/oauth/callback"
+CALLBACK_URL = "http://localhost:9292"
 
 Instagram.configure do |config|
   config.client_id = ENV['INSTA_CLIENT_ID']
@@ -19,7 +19,7 @@ Instagram.configure do |config|
 end
 
 get "/" do
-  '<a href="/oauth/connect">Connect with Instagram</a>'
+  erb :index
 end
 
 get "/oauth/connect" do
@@ -29,5 +29,4 @@ end
 get "/oauth/callback" do
   response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
   session[:access_token] = response.access_token
-  redirect "/nav"
 end
