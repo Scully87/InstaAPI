f2a55a37941f3fc456c542e352227914eef2d0ba
Scully87 <scully87.ts@googlemail.com>
2015-01-15 12:48:34 +0000

    created views for nav

--- app/server.rb
+++ app/server.rb
@@ -29,4 +29,9 @@ end
 get "/oauth/callback" do
   response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
   session[:access_token] = response.access_token
+  redirect "/nav"
+end
+
+get "/nav" do
+  erb :nav
 end
