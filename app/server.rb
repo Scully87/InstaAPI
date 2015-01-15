0d44bbda7db1bdee7a8f471a11d9fbb413d9fb74
Scully87 <scully87.ts@googlemail.com>
2015-01-15 12:20:19 +0000

    dotenv working and connecting keys

--- app/server.rb
+++ app/server.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'sinatra/partial'
 require 'instagram'
+require 'dotenv'
+Dotenv.load
 
 set :partial_template_engine, :erb
 set :public_folder, Proc.new { File.join(root, '..', 'public') }
@@ -10,8 +12,22 @@ enable :sessions
 CALLBACK_URL = "http://localhost:9292/oauth/callback"
 
 Instagram.configure do |config|
-  config.client_id = "INSTA_CLIENT_ID"
-  config.client_secret = "INSTA_CLIENT_SECRET"
+  config.client_id = ENV['INSTA_CLIENT_ID']
+  config.client_secret = ENV['INSTA_CLIENT_SECRET']
   # For secured endpoints only
   #config.client_ips = '<Comma separated list of IPs>'
+end
+
+get "/" do
+  '<a href="/oauth/connect">Connect with Instagram</a>'
+end
+
+get "/oauth/connect" do
+  redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+end
+
+get "/oauth/callback" do
+  response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+  session[:access_token] = response.access_token
+  redirect "/nav"
 end
--- spec/features/instagram_feature_spec.rb
+++ spec/features/instagram_feature_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
-require 'airborne'
 
-  describe 'sample spec' do
+  describe 'Instgram' do
     it 'should validate types' do
       get 'http://example.com/api/v1/simple_get' #json api that returns { "name" : "John Doe" }
       expect_json_types({name: :string})
