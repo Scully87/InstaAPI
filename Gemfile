5c899edaf8f8bcef9938e75a08c0ebd77313eb4f
Scully87 <scully87.ts@googlemail.com>
2015-01-15 12:02:11 +0000

    created spec file, added instagram config for keys

--- Gemfile
+++ Gemfile
@@ -4,3 +4,9 @@ gem 'rack'
 gem 'sinatra'
 gem 'sinatra-partial'
 gem 'instagram'
+gem 'dotenv'
+
+group :development, :test do
+  gem 'rspec'
+  gem 'capybara'
+end
--- app/server.rb
+++ app/server.rb
@@ -1,9 +1,17 @@
 require 'sinatra'
 require 'sinatra/partial'
+require 'instagram'
 
 set :partial_template_engine, :erb
 set :public_folder, Proc.new { File.join(root, '..', 'public') }
 
-get '/' do
-  erb :index
+enable :sessions
+
+CALLBACK_URL = "http://localhost:9292/oauth/callback"
+
+Instagram.configure do |config|
+  config.client_id = "INSTA_CLIENT_ID"
+  config.client_secret = "INSTA_CLIENT_SECRET"
+  # For secured endpoints only
+  #config.client_ips = '<Comma separated list of IPs>'
 end
