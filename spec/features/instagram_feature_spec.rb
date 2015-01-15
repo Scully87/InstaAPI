0ba6bdd84f9facc00cd1d3de63f9aa09bf9c9c12
Scully87 <scully87.ts@googlemail.com>
2015-01-15 17:18:13 +0000

    added feature test for visiting Nav

--- spec/features/instagram_feature_spec.rb
+++ spec/features/instagram_feature_spec.rb
@@ -8,11 +8,17 @@ feature 'Instgram' do
       expect(page).to have_link 'Connect with Instagram'
     end
 
-    scenario 'should authorise user' do
+    scenario 'should take user to authorise page' do
       visit '/'
       click_link 'Connect with Instagram'
       expect(current_path).to eq '/oauth/authorize/'
     end
-  
+
+    scenario 'once authorised user should see list of functions' do
+      visit '/nav'
+      expect(page).to have_content 'INSTA STUFF'
+    end
   end
+
+
 end
