323d8150910071f90b54f3b32a227bcd9bf53b2e
Scully87 <scully87.ts@googlemail.com>
2015-01-16 13:59:14 +0000

    added unlikes to likes controller

--- app/controllers/likes.rb
+++ app/controllers/likes.rb
@@ -3,3 +3,9 @@ get '/media_like/:id' do
   @client.like_media("#{params[:id]}")
   redirect "/user_recent_media"
 end
+
+get '/media_unlike/:id' do
+  client = Instagram.client(:access_token => session[:access_token])
+  client.unlike_media("#{params[:id]}")
+  redirect "/user_recent_media"
+end
